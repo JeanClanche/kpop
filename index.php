9f0facdeb60ba29d5f1983b4193f05de3a640a2b
Pedro <pedro@mail.com>
2026-03-31 12:59:52 +0200

    kré

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr" data-bs-theme="dark">
     <head>
         <meta charset="UTF-8">        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>Kpop - Connexion</title>
-    </head>
-    <body>
-        
 
+        <script src="bootstrap-5.3.8/js/bootstrap.js"></script>
+        <link rel="stylesheet" href="bootstrap-5.3.8/css/bootstrap.css">
+    </head>
+    <body class="">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-4 mt-5 shadow rounded border p-5">
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
