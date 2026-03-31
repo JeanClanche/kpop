8b56663a1b61c8068753c8fa15d9bd360e605c8c
Pedro <pedro@mail.com>
2026-03-31 14:29:20 +0200

    soigné

--- js/boutique.js
+++ js/boutique.js
@@ -1,4 +1,35 @@
 document.addEventListener('DOMContentLoaded', async function(){
-    const produits = await fetch('data/produits.json')
-    console.log(JSON.parse(await produits.text()))
+    const json = await fetch('data/produits.json')
+    const produits = JSON.parse(await json.text())
+    remplirBoutique(await produits)
 })
+
+function remplirBoutique(arr){
+    const affichage = document.getElementById('affichage')
+
+    arr.forEach(e => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.setAttribute('style', 'width : 18rem;')
+
+        const img = document.createElement('img')
+        img.classList.add('card-img-top')
+        img.setAttribute('src', e['pic'])
+
+        const body = document.createElement('div')
+        body.classList.add('card-body')
+
+        const titre = document.createElement('h5')
+        titre.textContent = e['nom']
+        titre.classList.add('card-title')
+
+        const desc = document.createElement('p')
+        desc.textContent = e['desc']
+        desc.classList.add('card-text')
+
+        body.append(titre, desc)
+        card.append(img, body)
+
+        affichage.append(card)
+    });
+}
